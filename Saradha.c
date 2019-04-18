162ced8382846d44f19971ad8eb108f29a6a383c
shradhaaggarwal03 <49750811+shradhaaggarwal03@users.noreply.github.com>
2019-04-18 21:50:59 +0530

    Add files via upload

---
+++ Saradha.c
@@ -0,0 +1,182 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int i,opt;
+struct Memory
+{
+	int M_ID,size,left,once_used;
+};
+
+struct Process
+{
+	int length,got_memory;
+};
+
+void First_Fit(struct Memory M[5],struct Process P[4])
+{
+	if(opt==1)
+	printf("      Inside first_fit  With Fixed_Length");
+	else
+	printf("      Inside first_fit  With Variable_Length");
+	
+	int i,j;
+	int n=4,m=5;
+	
+		for(i=0;i<n;i++)//loop for picking process
+		{
+			for(j=0;j<m;j++)//loop for seeking the best first memory
+			{
+				if(M[j].left>=P[i].length && P[i].got_memory==0 && M[j].once_used==0) 
+				{
+					M[j].left-=P[i].length;         //calculating left memory
+					printf("\n\nMemory NO        : %d",j+1);
+					printf("\nProcess NO       : %d",i+1);
+					printf("\nProcess Size     : %d",P[i].length);
+					printf("\nOriginal Space   : %d",M[j].size);
+					printf("\nSpace Occupied   : %d",P[i].length);
+					printf("\nSpace Left       : %d",M[j].left);
+					P[i].got_memory=1;
+					if(opt==1)
+					M[j].once_used=1;
+				}
+			}
+			if(P[i].got_memory==0)
+			{
+				//printf("\nNo Memory Available for Process: %d\n",i+1);
+			}	
+     	}
+}
+
+void Best_Fit(struct Memory M[5],struct Process P[4])
+{
+	if(opt==1)
+	printf("      Inside Best_Fit  With Fixed_Length");
+	else
+	printf("      Inside Best_Fit  With Variable_Length");
+	
+	int i,j,temp_j;
+	int n=4,m=5;
+	
+		for(i=0;i<n;i++)//loop for picking process
+		{
+			int min=999;
+			for(j=0;j<m;j++)//loop for seeking the best first memory
+			{
+				if(M[j].left>=P[i].length && M[j].left<min && P[i].got_memory==0 && M[j].once_used==0)//checking for the best memory
+				{
+					min=M[j].left;
+					temp_j=j;
+				}	
+			}
+			
+			if(M[temp_j].left>=P[i].length && P[i].got_memory==0 && M[temp_j].once_used==0) 
+				{
+					M[temp_j].left-=P[i].length;         //calculating left memory
+					printf("\n\nMemory NO        : %d",temp_j+1);
+					printf("\nProcess NO       : %d",i+1);
+					printf("\nProcess Size     : %d",P[i].length);
+					printf("\nOriginal Space   : %d",M[temp_j].size);
+					printf("\nSpace Occupied   : %d",P[i].length);
+					printf("\nSpace Left       : %d",M[temp_j].left);
+					P[i].got_memory=1;
+					if(opt==1)
+					M[temp_j].once_used=1;
+				}
+     	}
+}
+
+void Worst_Fit(struct Memory M[5],struct Process P[4])
+{
+	if(opt==1)
+	printf("      Inside Worst_Fit  With Fixed_Length");
+	else
+	printf("      Inside Worst_Fit  With Variable_Length");
+	
+	int i,j,temp_j;
+	int n=4,m=5;
+	
+		for(i=0;i<n;i++)//loop for picking process
+		{
+			int max=0;
+			for(j=0;j<m;j++)//loop for seeking the best first memory
+			{
+				if(M[j].left>=P[i].length && M[j].left>max && P[i].got_memory==0 && M[j].once_used==0)//checking for the best memory
+				{
+					max=M[j].left;
+					temp_j=j;
+				}	
+			}
+			
+			if(M[temp_j].left>=P[i].length && P[i].got_memory==0 && M[temp_j].once_used==0) 
+				{
+					M[temp_j].left-=P[i].length;         //calculating left memory
+					printf("\n\nMemory NO        : %d",temp_j+1);
+					printf("\nProcess NO       : %d",i+1);
+					printf("\nProcess Size     : %d",P[i].length);
+					printf("\nOriginal Space   : %d",M[temp_j].size);
+					printf("\nSpace Occupied   : %d",P[i].length);
+					printf("\nSpace Left       : %d",M[temp_j].left);
+					P[i].got_memory=1;
+					if(opt==1)
+					M[temp_j].once_used=1;
+				}
+     	}
+}
+
+int main()
+{
+	//initalizing all the value
+	struct Memory M[5];
+	M[0].size=100;
+	M[1].size=500;
+	M[2].size=200;
+	M[3].size=300;
+	M[4].size=600;
+	
+	struct Process P[4];
+	P[0].length=212;
+	P[1].length=417;
+	P[2].length=112;
+	P[3].length=426;
+	
+	for(i=0;i<5;i++)//initalizing memory
+	{
+		M[i].left=M[i].size;
+		M[i].once_used=0;
+	}
+	
+	for(i=0;i<4;i++)//initalizing process
+	{
+		P[i].got_memory=0;
+	}
+	
+	int choice;
+	printf("Select The Option");
+	printf("\nPress 1: For First_Fit");
+	printf("\nPress 2: For Best_Fit");
+	printf("\nPress 3: For Worst_Fit\n");
+	scanf("%d",&choice);
+	
+	system("cls");
+	printf("Select The Partioning Method");
+	printf("\nPress 1: For Fixed_Length");
+	printf("\nPress 2: For Variable_Length\n");
+	scanf("%d",&opt);
+	
+	system("cls");
+	switch(choice)
+	{
+		case 1:
+			First_Fit(M,P);
+			break;
+			
+		case 2:
+			Best_Fit(M,P);
+			break;
+			
+		case 3:
+			Worst_Fit(M,P);
+			break;		
+	}
+}
---
+++ que2.c
@@ -0,0 +1,162 @@
+#include<stdio.h>j
+	void check_at();
+
+	int main()
+{
+
+	int n, time=0,i=0,j,k=0,total_bt=0,temp1,temp2,temp3,tt;
+	int q1=3,q2=6;
+	int front=0,f=0;
+	int rear=0,r=0;
+	int at[i],bt[i],rbt[i],wt[i],ctime[i],tat[i];
+	int flag[i];
+	flag[0]=0;
+	int temp_rbt[k],t[i];
+	int queue_1[total_bt];		// first queue
+	int queue_2[total_bt];
+	int q_3[total_bt];
+	printf("\nEnter the no of process: ");
+	scanf("%d",&n);					//total no of process
+	for(i=0;i<=n-1;i++){				//entering at and bt
+	printf("\nEnter Arrival Time and Burst Time for process P[%d]: ",i);
+		scanf("%d%d", &at[i],&bt[i]);
+		rbt[i]=bt[i];
+		total_bt=total_bt+bt[i];
+		}
+	
+
+	
+
+	
+
+	printf("\n\tProcess\t|\tArival Time\t|\tBurst Time\t|");
+	//sorting process in accending ordre wrt at
+	for (i=0 ; i<=n-1;i++){
+		for (j=0 ; j<n-i-1 ; j++){
+			if(at[j]>at[j+1] ){
+	printf("1\n");
+				temp1=at[j];
+				temp2=rbt[j];
+				temp3=bt[j];
+				at[j]=at[j+1];
+				rbt[j]=rbt[j+1];
+				bt[j]=bt[j+1];
+				at[j+1]=temp1;
+				rbt[j+1]=temp2;
+				bt[j+1]=temp3;
+				}
+			}
+	}
+	//adding first process to queue1
+	if(rear==total_bt){
+		printf("\nOverflow");
+		}
+	else{
+		printf("\nfirst process entered.");
+		rear=rear+1;
+		queue_1[rear]=rbt[0];
+		printf("]\nqueue_1=%d added",queue_1[rear]);
+	printf("\n2");
+		}
+	//process scheduling initiated
+	i=0;
+	printf("\n1flag[%d]: %d",i,flag[i]);
+	do{
+	if(front>=rear){
+		printf("\nfront :%d\nrear :%d",front,rear);
+		printf("\nunderflow");
+		
+		}
+	else{
+		rbt[i]=queue_1[front];
+		front=front+1;
+		if(flag[i]==0){
+			
+			//if process traversed first time
+			if(rbt[i]>q1){
+				printf("\n1flag[%d]: %d",i,flag[i]);
+				time=time+q1;
+				rbt[i]=rbt[i]-q1;
+				flag[i]++;
+				
+				for(j=front+1;j<n;j++){
+					if(at[j]<=time){
+					rear=rear+1;
+					queue_1[rear]=rbt[j];
+					
+					}
+				}
+				
+				rear=rear+1;
+				queue_1[rear]=rbt[i];
+				
+			}
+			//if process traversed second time
+			else{
+				printf("\n1flag[%d]: %d",i,flag[i]);
+				time=time+rbt[i];
+				
+				for(j=front+1;j<n;j++){
+					if(at[j]<=time){
+					rear=rear+1;
+					queue_1[rear]=rbt[j];
+					printf("10\n");
+					}
+				}
+				
+				flag[i]++;
+				ctime[i]=time;
+				tat[i]=ctime[i]-at[i];
+				wt[i]=tat[i]-bt[i];
+			}
+			
+			
+		//	printf("\t%d\t\t%d\t\t%d\t\t%d\t\t%d",i,at[i],bt[i],ctime[i],tat[i],wt[i]);
+	
+
+		}
+	
+
+		else if(flag[i]==1){
+			//if process traversed first time
+			
+			if(rbt[i]>q2){
+				printf("\n1flag[%d]: %d",i,flag[i]);
+				time=time+q2;
+				rbt[i]=rbt[i]-q2;
+				
+				for(j=front+1;j<n;j++){
+					if(at[j]<=time){
+					rear=rear+1;
+					queue_1[rear]=rbt[j];
+					printf("10\n");
+					}
+				}
+				
+				flag[i]++;
+				rear=rear+1;
+				queue_1[rear]=rbt[i];
+				
+	
+
+			}
+			//if process traversed second time
+			else{
+				printf("\n1flag[%d]: %d",i,flag[i]);
+				time=time+rbt[i];
+				flag[i]++;
+				
+				for(j=front+1;j<n;j++){
+					if(at[j]<=time){
+					rear=rear+1;
+					queue_1[rear]=rbt[j];
+					printf("10\n");
+					}
+				}
+				
+				ctime[i]=time;
+				tat[i]=ctime[i]-at[i];
+				wt[i]=tat[i]-bt[i];
+			//	printf("\t%d\t\t%d\t\t%d\t\t%d\t\t%d",i,at[i],bt[i],ctime[i],tat[i],wt[i]);
+			
+
